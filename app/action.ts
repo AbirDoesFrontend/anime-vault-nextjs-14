dabf269c60fa786308188c0ab8eb17d58b5a62d1
Abir Khan <abirkhan.frontend.dev24@gmail.com>
2023-12-03 21:40:29 +0600

    Infinite Scroll - Intersection Observer

--- app/action.ts
+++ app/action.ts
@@ -1,7 +1,7 @@
 "use server";
 
 export const fetchAnime = async (page : number) => {
-
+    
     const url = `https://shikimori.one/api/animes?page=${page}&limit=8&order=popularity`;
 
     const response = await fetch(url);
